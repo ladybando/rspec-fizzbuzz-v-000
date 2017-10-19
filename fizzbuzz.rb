0af6bac66557f815395d3301207c6284c0dc59bf
Ayana Bando <ayanaflax@yahoo.com>
2017-10-19 17:49:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+    "FizzBuzz"
 
   elsif int % 5 == 0
-    return "Buzz"
+    "Buzz"
   elsif int % 3 == 0
-    return "Fizz"
+    "Fizz"
   elsif int !(%3 && 5) == 0
-    returns "nil"  
+    "nil"
   end
 end
