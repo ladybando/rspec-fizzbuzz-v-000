2d93fdbf13b6d13ff4368168cae963ed275ef8f6
Ayana Bando <ayanaflax@yahoo.com>
2017-10-19 17:47:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,9 @@ def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
 
-  if int % 5 == 0
+  elsif int % 5 == 0
     return "Buzz"
-
-
-  if int % 3 == 0
+else  int % 3 == 0
     return "Fizz"
   end
 end
